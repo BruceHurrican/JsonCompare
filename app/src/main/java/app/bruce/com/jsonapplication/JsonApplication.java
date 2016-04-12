a4cb9db70522058ce033c0a0c64a28434d2bc4a9
bruceHome <313028033@qq.com>
2016-04-12 13:53:46 +0800

    update

---
+++ app/src/main/java/app/bruce/com/jsonapplication/JsonApplication.java
@@ -0,0 +1,9 @@
+package app.bruce.com.jsonapplication;
+
+import com.bruceutils.base.BaseApplication;
+
+/**
+ * Created by BruceHurrican on 2016/4/12.
+ */
+public class JsonApplication extends BaseApplication {
+}
